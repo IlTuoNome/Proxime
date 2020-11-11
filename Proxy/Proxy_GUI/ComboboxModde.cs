484e70cdef96867d7bcb0a8f98a0c24bf072ee49
danypsnpsn <danypsnpsn@hotmail.com>
2020-11-11 16:13:52 +0100

    Aggiungere i file di progetto.

---
+++ Proxy/Proxy_GUI/ComboboxModde.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proxy_GUI
+{
+    /// <summary>
+    /// Class for the modified combobox
+    /// </summary>
+    public class ComboboxModde : ComboBox
+    {
+        static ComboboxModde()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ComboboxModde), new FrameworkPropertyMetadata(typeof(ComboboxModde)));
+        }
+
+        /// <summary>
+        /// Properties for displaying text after removal in the list
+        /// </summary>
+        public Visibility Lvis
+        {
+            get { return (Visibility)GetValue(LvisProperty); }
+            set { SetValue(LvisProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Lvis.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LvisProperty =
+            DependencyProperty.Register("Lvis", typeof(Visibility), typeof(ComboboxModde), new PropertyMetadata(Visibility.Hidden));
+
+
+        /// <summary>
+        /// Properties to set the text to be displayed
+        /// </summary>
+        public string Ltext
+        {
+            get { return (string)GetValue(LtextProperty); }
+            set { SetValue(LtextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Ltext.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LtextProperty =
+            DependencyProperty.Register("Ltext", typeof(string), typeof(ComboboxModde), new PropertyMetadata(""));
+    }
+}
---
+++ Proxy/Proxy_GUI/EventClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Proxy_GUI
+{
+    /// <summary>
+    /// Class to handle events related to the Generic.xaml file
+    /// </summary>
+    partial class EventClass
+    {
+        /// <summary>
+        /// Field for converting brush colors
+        /// </summary>
+        private BrushConverter BrushConverter = new BrushConverter();
+
+        /// <summary>
+        /// Method to handle the mouseover event on the combobox toggle
+        /// </summary>
+        private void Border_Toggle_MouseEvent(object sender, MouseEventArgs e)
+        {
+            Border border = (Border)sender;
+            switch (border.IsMouseOver)
+            {
+                case true:
+                    border.Background = (Brush)BrushConverter.ConvertFrom("#0061FF");
+                    break;
+                case false:
+                    border.Background = (Brush)BrushConverter.ConvertFrom("#369FFF");
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Method to manage the mouse click event on the combobox toggle
+        /// </summary>
+        private void Border_Toggle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Border border = (Border)sender;
+            border.MouseEnter -= Border_Toggle_MouseEvent;
+            border.MouseLeave -= Border_Toggle_MouseEvent;
+            border.Background = (Brush)BrushConverter.ConvertFrom("#B20061FF");
+        }
+
+        /// <summary>
+        /// Method to manage the release event click mouse on the combobox toggle
+        /// </summary>
+        private void Border_Toggle_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Border border = (Border)sender;
+            border.MouseEnter += Border_Toggle_MouseEvent;
+            border.MouseLeave += Border_Toggle_MouseEvent;
+            Border_Toggle_MouseEvent(border, e);
+        }
+
+    }
+}
